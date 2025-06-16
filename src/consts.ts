a541a7bdab25cfebe3af91e46e7e85e37f5919e3
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2025-06-16 20:55:52 +0700

    update name

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Sigma Logistics";
+export const SITE_TITLE = "Sigma Distribusi";
 export const SITE_DESCRIPTION =
   "Strategic distribution partner for large-scale goods and logistics solutions across the nation.";
